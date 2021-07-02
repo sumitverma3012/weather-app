a2d08e8b4f9b954e39e0c04eff78164cda7f31b8
Sumit V <sumit.v@logmein.com>
2021-07-02 20:01:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Weather from './weather';
+import './index.css';
+function App() {
+  return <Weather />
+}
+
+export default App;
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const api = {
+    key: 'd8f57795a8cf3d47a1c31478b9fb5cbe',
+    base: 'http://api.openweathermap.org/data/2.5/'
+}
